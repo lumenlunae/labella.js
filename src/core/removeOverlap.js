263c9ba029876132bab845ee958d8f9b9159442f
Matt <matt@signalblue.net>
2019-06-29 16:33:50 -0700

    Add optional functions for layerGap and nodeSpacing

--- src/core/removeOverlap.js
+++ src/core/removeOverlap.js
@@ -1,74 +1,83 @@
-const helper = require('./helper.js');
-const vpsc = require('../lib/vpsc.js');
+const helper = require("./helper.js");
+const vpsc = require("../lib/vpsc.js");
 
 const DEFAULT_OPTIONS = {
   lineSpacing: 2,
   nodeSpacing: 3,
   minPos: 0,
-  maxPos: null
+  maxPos: null,
+  nodeSpacingFn: undefined
 };
 
-function nodeToVariable(node){
+function nodeToVariable(node) {
   const v = new vpsc.Variable(node.targetPos);
   v.node = node;
   return v;
 }
 
-function removeOverlap(nodes, options){
-  if(nodes.length>0){
+function removeOverlap(nodes, options) {
+  if (nodes.length > 0) {
     options = helper.extend(DEFAULT_OPTIONS, options);
 
     // For nodes with stub, set target position to stub's current position
-    nodes.forEach(function (node, index) {
+    nodes.forEach(function(node, index) {
       node.targetPos = node.parent ? node.parent.currentPos : node.idealPos;
       node.index = index;
     });
 
-    const variables = nodes.concat()
-      .sort(function (a, b) {
+    const variables = nodes
+      .concat()
+      .sort(function(a, b) {
         const diff = a.targetPos - b.targetPos;
         if (diff !== 0) return diff;
         const diff2 = a.isStub() - b.isStub();
-        if (diff2!== 0) return diff2;
+        if (diff2 !== 0) return diff2;
         // If same position, use original order
-        return a.index-b.index;
+        return a.index - b.index;
       })
       .map(nodeToVariable);
 
     const constraints = [];
-    for(let i=1;i<variables.length;i++){
-      const v1 = variables[i-1];
+    for (let i = 1; i < variables.length; i++) {
+      const v1 = variables[i - 1];
       const v2 = variables[i];
 
       let gap;
-      if(v1.node.isStub() && v2.node.isStub()){
-        gap = (v1.node.width+v2.node.width)/2 + options.lineSpacing;
+      if (v1.node.isStub() && v2.node.isStub()) {
+        gap = (v1.node.width + v2.node.width) / 2 + options.lineSpacing;
+      } else {
+        gap = (v1.node.width + v2.node.width) / 2 + options.nodeSpacing;
       }
-      else{
-        gap = (v1.node.width+v2.node.width)/2 + options.nodeSpacing;
+      if (options.nodeSpacingFn) {
+        gap = options.nodeSpacingFn(v1, v2);
       }
-      constraints.push(new vpsc.Constraint(v1, v2, gap));
+      if (gap) {
+        constraints.push(new vpsc.Constraint(v1, v2, gap));
+      }
     }
 
-    if(helper.isDefined(options.minPos)){
+    if (helper.isDefined(options.minPos)) {
       const leftWall = new vpsc.Variable(options.minPos, 1e10);
       const v = variables[0];
-      constraints.push(new vpsc.Constraint(leftWall, v, v.node.width/2));
+      constraints.push(new vpsc.Constraint(leftWall, v, v.node.width / 2));
       variables.unshift(leftWall);
     }
 
-    if(helper.isDefined(options.maxPos)){
+    if (helper.isDefined(options.maxPos)) {
       const rightWall = new vpsc.Variable(options.maxPos, 1e10);
       const lastv = helper.last(variables);
-      constraints.push(new vpsc.Constraint(lastv, rightWall, lastv.node.width/2));
+      constraints.push(
+        new vpsc.Constraint(lastv, rightWall, lastv.node.width / 2)
+      );
       variables.push(rightWall);
     }
-
-    (new vpsc.Solver(variables, constraints)).solve();
+    console.log(variables);
+    console.log(constraints);
+    new vpsc.Solver(variables, constraints).solve();
 
     variables
-      .filter( v => v.node )
-      .map(function(v){
+      .filter(v => v.node)
+      .map(function(v) {
         v.node.currentPos = Math.round(v.position());
         return v;
       });
--- src/core/renderer.js
+++ src/core/renderer.js
@@ -1,41 +1,37 @@
-const helper = require('./helper.js');
+const helper = require("./helper.js");
 
-function Renderer(options){
-  this.options = helper.extend({
-    layerGap: 60,
-    nodeHeight: 10,
-    direction: 'down'
-  }, options);
+function Renderer(options) {
+  this.options = helper.extend(
+    {
+      layerGap: 60,
+      layerGapFn: undefined,
+      nodeHeight: 10,
+      direction: "down"
+    },
+    options
+  );
 }
 
-function lineTo(point){
-  return 'L ' + point.join(' ');
+function lineTo(point) {
+  return "L " + point.join(" ");
 }
 
-function moveTo(point){
-  return 'M ' + point.join(' ');
+function moveTo(point) {
+  return "M " + point.join(" ");
 }
 
-function curveTo(c1, c2, point2){
-  return 'C ' + c1.join(' ') + ' ' + c2.join(' ') + ' ' + point2.join(' ');
+function curveTo(c1, c2, point2) {
+  return "C " + c1.join(" ") + " " + c2.join(" ") + " " + point2.join(" ");
 }
 
-function vCurveBetween(point1, point2){
+function vCurveBetween(point1, point2) {
   const midY = (point1[1] + point2[1]) / 2;
-  return curveTo(
-    [point1[0], midY],
-    [point2[0], midY],
-    point2
-  );
+  return curveTo([point1[0], midY], [point2[0], midY], point2);
 }
 
-function hCurveBetween(point1, point2){
+function hCurveBetween(point1, point2) {
   const midX = (point1[0] + point2[0]) / 2;
-  return curveTo(
-    [midX, point1[1]],
-    [midX, point2[1]],
-    point2
-  );
+  return curveTo([midX, point1[1]], [midX, point2[1]], point2);
 }
 
 Renderer.lineTo = lineTo;
@@ -44,78 +40,98 @@ Renderer.curveTo = curveTo;
 Renderer.vCurveBetween = vCurveBetween;
 Renderer.hCurveBetween = hCurveBetween;
 
-Renderer.prototype.getWaypoints = function(node){
+Renderer.prototype.getWaypoints = function(node) {
   var options = this.options;
   var direction = options.direction;
 
   var hops = node.getPathFromRoot();
-  var gap = options.nodeHeight + options.layerGap;
+  var layerGap = options.layerGapFn
+    ? options.layerGapFn(node)
+    : options.layerGap;
 
-  if(direction==='left'){
-    return [[[0, hops[0].idealPos]]].concat(hops.map(function(hop, level){
-      var xPos = gap * (level+1) * -1;
-      return [
-        [xPos + options.nodeHeight, hop.currentPos],
-        [xPos, hop.currentPos]
-      ];
-    }));
-  }
-  else if(direction==='right'){
-    return [[[0, hops[0].idealPos]]].concat(hops.map(function(hop, level){
-      var xPos = gap * (level+1);
-      return [
-        [xPos - options.nodeHeight, hop.currentPos],
-        [xPos, hop.currentPos]
-      ];
-    }));
-  }
-  else if(direction==='up'){
-    return [[[hops[0].idealPos, 0]]].concat(hops.map(function(hop, level){
-      var yPos = gap * (level+1) * -1;
-      return [
-        [hop.currentPos, yPos + options.nodeHeight],
-        [hop.currentPos, yPos]
-      ];
-    }));
-  }
-  else{
-    return [[[hops[0].idealPos, 0]]].concat(hops.map(function(hop, level){
-      var yPos = gap * (level+1);
-      return [
-        [hop.currentPos, yPos - options.nodeHeight],
-        [hop.currentPos, yPos]
-      ];
-    }));
+  var gap = options.nodeHeight + layerGap;
+
+  if (direction === "left") {
+    return [[[0, hops[0].idealPos]]].concat(
+      hops.map(function(hop, level) {
+        var xPos = gap * (level + 1) * -1;
+        return [
+          [xPos + options.nodeHeight, hop.currentPos],
+          [xPos, hop.currentPos]
+        ];
+      })
+    );
+  } else if (direction === "right") {
+    return [[[0, hops[0].idealPos]]].concat(
+      hops.map(function(hop, level) {
+        var xPos = gap * (level + 1);
+        return [
+          [xPos - options.nodeHeight, hop.currentPos],
+          [xPos, hop.currentPos]
+        ];
+      })
+    );
+  } else if (direction === "up") {
+    return [[[hops[0].idealPos, 0]]].concat(
+      hops.map(function(hop, level) {
+        var yPos = gap * (level + 1) * -1;
+        return [
+          [hop.currentPos, yPos + options.nodeHeight],
+          [hop.currentPos, yPos]
+        ];
+      })
+    );
+  } else {
+    return [[[hops[0].idealPos, 0]]].concat(
+      hops.map(function(hop, level) {
+        var yPos = gap * (level + 1);
+        return [
+          [hop.currentPos, yPos - options.nodeHeight],
+          [hop.currentPos, yPos]
+        ];
+      })
+    );
   }
 };
 
-Renderer.prototype.layout = function(nodes){
+Renderer.prototype.layout = function(nodes) {
   var options = this.options;
 
   var gap = options.layerGap + options.nodeHeight;
 
-  switch(options.direction){
-    case 'left':
-      nodes.forEach(function(node){
-        var pos = node.getLayerIndex() * gap + options.layerGap;
+  var posFn = function(node) {
+    var layerGap;
+    if (options.layerGapFn) {
+      layerGap = options.layerGapFn(node);
+    } else {
+      layerGap = options.layerGap;
+    }
+    var gap = layerGap + options.nodeHeight;
+    return node.getLayerIndex() * gap + layerGap;
+  };
+
+  switch (options.direction) {
+    case "left":
+      nodes.forEach(function(node) {
+        var pos = posFn(node);
         node.x = -pos - options.nodeHeight;
         node.y = node.currentPos;
         node.dx = options.nodeHeight;
         node.dy = node.width;
       });
       break;
-    case 'right':
-      nodes.forEach(function(node){
-        var pos = node.getLayerIndex() * gap + options.layerGap;
+    case "right":
+      nodes.forEach(function(node) {
+        var pos = posFn(node);
         node.x = pos;
         node.y = node.currentPos;
         node.dx = options.nodeHeight;
         node.dy = node.width;
       });
       break;
-    case 'up':
-      nodes.forEach(function(node){
-        var pos = node.getLayerIndex() * gap + options.layerGap;
+    case "up":
+      nodes.forEach(function(node) {
+        var pos = posFn(node);
         node.x = node.currentPos;
         node.y = -pos - options.nodeHeight;
         node.dx = node.width;
@@ -123,9 +139,9 @@ Renderer.prototype.layout = function(nodes){
       });
       break;
     default:
-    case 'down':
-      nodes.forEach(function(node){
-        var pos = node.getLayerIndex() * gap + options.layerGap;
+    case "down":
+      nodes.forEach(function(node) {
+        var pos = posFn(node);
         node.x = node.currentPos;
         node.y = pos;
         node.dx = node.width;
@@ -137,7 +153,7 @@ Renderer.prototype.layout = function(nodes){
   return nodes;
 };
 
-Renderer.prototype.generatePath = function(node){
+Renderer.prototype.generatePath = function(node) {
   var options = this.options;
   var direction = options.direction;
 
@@ -145,26 +161,25 @@ Renderer.prototype.generatePath = function(node){
 
   var steps = [moveTo(waypoints[0][0])];
 
-  if(direction==='left' || direction==='right'){
-    waypoints.reduce(function(prev, current, level){
-      steps.push(hCurveBetween(prev[prev.length-1], current[0]));
-      if(level < waypoints.length-1){
+  if (direction === "left" || direction === "right") {
+    waypoints.reduce(function(prev, current, level) {
+      steps.push(hCurveBetween(prev[prev.length - 1], current[0]));
+      if (level < waypoints.length - 1) {
         steps.push(lineTo(current[1]));
       }
       return current;
     });
-  }
-  else{
-    waypoints.reduce(function(prev, current, level){
-      steps.push(vCurveBetween(prev[prev.length-1], current[0]));
-      if(level < waypoints.length-1){
+  } else {
+    waypoints.reduce(function(prev, current, level) {
+      steps.push(vCurveBetween(prev[prev.length - 1], current[0]));
+      if (level < waypoints.length - 1) {
         steps.push(lineTo(current[1]));
       }
       return current;
     });
   }
 
-  return steps.join(' ');
+  return steps.join(" ");
 };
 
 // return module
